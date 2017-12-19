c2eacb08bac59fdde036f1e8ce3fe29937cfa6a1
xiaohei <991807272@qq.com>
2017-12-19 15:29:05 +0800

    #修改# 我的收藏页面逻辑

--- js/my-collection.js
+++ js/my-collection.js
@@ -1,20 +1,35 @@
-$(function(){
+$((function(){
 	// 轮播图初始化
-	// var mySwiper = new Swiper ('.swiper-container', {
-	//   pagination: '.swiper-pagination',
- //    nextButton: '.swiper-button-next',
- //    prevButton: '.swiper-button-prev',
- //    paginationClickable: true,
- //    centeredSlides: true,
- //    autoplay: 6500,
- //    autoplayDisableOnInteraction: false
-	//  })
+	var mySwiper = new Swiper ('.swiper-container', {
+	  pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    paginationClickable: true,
+    centeredSlides: true,
+    // autoplay: 6500,
+    autoplayDisableOnInteraction: false,
+    //当前页面
+    onSlideChangeEnd : function(swiperHere) {
+      //swiperHere.activeIndex 为当前页面所在索引
+			let index = swiperHere.activeIndex;
 
+			//切换导航激活状态
+			$('.choose-btn ul li').removeClass('active');
+			$('.choose-btn ul li').eq(index).addClass('active');
 
-	//切换收藏内容：
-	$('.choose-btn ul li ').on('click',function(){
-		$(this).siblings().removeClass('active');
-		$(this).addClass('active');
-	});
+			let left = (index*167.5+105)/75;
+			console.log(left);
+			$('.active-border').css({"left":left+"rem"});
+     }
+	 })
 
-})
+	$('.swiper-pagination span').eq(0).text("班级课");
+	$('.swiper-pagination span').eq(1).text("校园课");
+	$('.swiper-pagination span').eq(2).text("作业");
+	$('.swiper-pagination span').eq(3).text("问题");
+
+	$('.swiper-pagination span').css({
+		"backgroundColor":"#fff"
+	})
+})());
+
