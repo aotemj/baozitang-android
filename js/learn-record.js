15be8e1c81504989f6ff965d389e8e17725b83de
xiaohei <991807272@qq.com>
2017-12-19 22:02:40 +0800

    #新增# 学习记录页面

---
+++ js/learn-record.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	//动态设置历史作业高度
+	var liLength = $('.history-home li').length;
+	var liHeight = $('.history-home li').height();
+	console.log(liLength);
+	console.log(liHeight);
+	var ulHeight = liLength*liHeight+800;
+	console.log(ulHeight);
+	$('.history-home').height(ulHeight/75+'rem');
+});
