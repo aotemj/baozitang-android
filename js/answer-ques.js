5cbbfcc3e55c39401a6deb46a72ac7b8ec143a6e
xiaohei <991807272@qq.com>
2017-12-20 11:56:08 +0800

    #新增# 问问题静态页面

---
+++ js/answer-ques.js
@@ -0,0 +1,14 @@
+$(function(){
+	//动态设置历史作业高度
+	// var liLength = $('.history-home li').length;
+	// var liHeight = $('.history-home li').height();
+	// console.log(liLength);
+	// console.log(liHeight);
+	// var ulHeight = liLength*liHeight+500;
+	// console.log(ulHeight);
+	// $('.history-home').height(ulHeight/75+'rem');
+
+		//动态获取问题盒子高度
+		var height = $(document.body).height();
+		$('.ques-list').height(height);
+});
