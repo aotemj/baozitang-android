cca5ad317be5d4828f23cf57ebe1033c385ca264
xiaohei <991807272@qq.com>
2017-12-21 10:34:00 +0800

    #新增 班级 班级 子页面#

---
+++ js/class.js
@@ -0,0 +1,38 @@
+$((function(){
+	// 轮播图初始化
+	var mySwiper = new Swiper ('.swiper-container', {
+	  pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    paginationClickable: true,
+    centeredSlides: true,
+    // autoplay: 6500,
+    autoplayDisableOnInteraction: false,
+    //当前页面
+    onSlideChangeEnd : function(swiperHere) {
+      //swiperHere.activeIndex 为当前页面所在索引
+			let index = swiperHere.activeIndex;
+
+			//切换导航激活状态
+			$('.choose-btn ul li').removeClass('active');
+			$('.choose-btn ul li').eq(index).addClass('active');
+
+			let left = (index*167.5+105)/75;
+			console.log(left);
+			$('.active-border').css({"left":left+"rem"});
+     }
+	 })
+
+	$('.swiper-pagination span').eq(0).text("班级");
+	$('.swiper-pagination span').eq(1).text("精华");
+	$('.swiper-pagination span').eq(2).text("作业");
+	$('.swiper-pagination span').eq(3).text("问题");
+
+	$('.swiper-pagination span').css({
+		"backgroundColor":"#fff"
+	})
+
+	//获取收藏课程：
+
+})());
+
