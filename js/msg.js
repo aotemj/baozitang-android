f00e874a2069e8b1c33e1210da90c8c4f298babd
xiaohei <991807272@qq.com>
2017-12-27 16:11:55 +0800

    #修改# 通知页面图标

--- js/msg.js
+++ js/msg.js
@@ -1,17 +1,2 @@
 $(function(){
-
-	//点击切换标签
-	// $('.sub-nav ul .comments').on('click',function(){
-	// 	$(this).siblings().removeClass('active');
-	// 	$(this).addClass('active');
-	// 	$('.inner-content .comments-box').addClass('active');
-	// 	$('.inner-content .msg-box').removeClass('active');
-	// });
-	// $('.sub-nav ul .msg').on('click',function(){
-	// 	$(this).siblings().removeClass('active');
-	// 	$(this).addClass('active');
-	// 	$('.inner-content .comments-box').removeClass('active');
-	// 	$('.inner-content .msg-box').addClass('active');
-	// });
-
 });
