e714ba557981fbb9d8acdf6c6fa5450636c33a71
zhaoxinlei <991807272@qq.com>
2017-12-31 17:17:39 +0800

    #修改# 问问页面数据交互，动态化

--- js/answer-ques.js
+++ js/answer-ques.js
@@ -1,143 +1,22 @@
-$(function(){
+$(function () {
+    //点击返回上一层目录
+    $('.back').on('click', function () {
+        window.baozitang.close();
+    });
 
+    // //动态显示问题输入框
+    $('.ask-btn').on('click', function () {
+        $('.ask-window').slideToggle().find('input').focus();
+    })
 
+    $('.close,.send').on('click', function () {
+        $('.ask-window').slideUp();
+    })
 
-	//点击返回上一层目录
-	$('.back').on('click',function(){
-		window.baozitang.close();
-	});
-
-	// //动态显示问题输入框
-	$('.ask-btn').on('click',function(){
-		$('.ask-window').slideToggle().find('input').focus();
-	})
-
-	//发送问题
-	// $('.send').on('click',function(){
-	// 	if($.trim($('.ask-window input').val())===""){
-	// 		mui.toast('请输入内容后提交！');
-	// 		return;
-	// 	}
-	// 	//获取问题标题和描述
-	// 	var title = $.trim($('.ask-window input').val());
-	// 	var desc = $.trim($('.ask-window textarea').val());
-
-	// 	askQues(
-	// 		{
-	// 			studentId:3,
-	// 			sectionId:1,
-	// 			classId:1,
-	// 			title:title,
-	// 			description:desc,
-	// 			category:'section',
-	// 		},
-	// 		function(res){
-	// 			if(res.code!=200){
-	// 				mui.toast(res.msg);
-	// 				return;
-	// 			}
-	// 			// $('.ques-list').removeClass('active');
-	// 			// console.log(res);
-	// 			mui.toast(res.msg+'1');
-	// 			//重新加载页面
-	// 			$('.ques-list ul').html('');
-	// 			loadAnswerList();
-	// 			mui.toast(res.msg+'2');
-	// 		});
-	// 	// 清空输入框
-	// 	$('.ask-window input').val("");
-	// 	$('.ask-window textarea').val("");
-	// });
-		$('.send').click(function(){
-		if($.trim($('.ask-window input').val())===""){
-			mui.toast('请输入内容后提交！');
-			return;
-		}
-		//获取问题标题和描述
-		var title = $.trim($('.ask-window input').val());
-		var desc = $.trim($('.ask-window textarea').val());
-
-		askQues(
-			{
-				studentId:3,
-				sectionId:1,
-				classId:1,
-				title:title,
-				description:desc,
-				category:'section',
-			},
-			function(res){
-				if(res.code!=200){
-					mui.toast(res.msg);
-					return;
-				}
-				// $('.ques-list').removeClass('active');
-				// console.log(res);
-				mui.toast(res.msg+'1');
-				//重新加载页面
-				$('.ques-list ul').html('');
-				loadAnswerList();
-				mui.toast(res.msg+'2');
-			});
-		// 清空输入框
-		$('.ask-window input').val("");
-		$('.ask-window textarea').val("");
-	});
-
-	$('.close,.send').on('click',function(){
-		$('.ask-window').slideUp();
-	})
-
-	//滚动时隐藏问题输入框
-	$(window).on('scroll',function(){
-		$('.ask-window').slideUp();
-	});
-
-	//页面初始化
-	loadAnswerList();
-	// 封装渲染页面方法
-	function loadAnswerList(){
-		//获取问题列表
-		getAnswerList(
-			//data
-			{
-				"studentId":"3",
-				"sectionId":"1",
-				"classId":"1"
-			},
-			//callback
-			function(res){
-				console.log(res);
-				//没有问题处理
-				if(res.data.length == 0){
-					$('.ques-list').addClass('active');
-					$('.ques-list>.no-con-prompt').addClass('active');
-					$('.answer').addClass('active').find('button').text('去提问');
-					return;
-				}
-				for(var i = 0;i<res.data.length;i++){
-					var data = res.data[i];
-					//无描述处理
-					if(data.description == undefined){
-						data.description='';
-					}
-					//问题创建时间处理
-					var date = timeFilter(data.createDate);
-
-					$('.ques-list ul').append(
-						'<li>'
-						+'	<a href="#">'
-						+'		<h4>'+data.title+'</h4>'
-						+'		<p>'+data.description+'</p>'
-						+'	</a>'
-						+'	<div class="comments">'
-						+'		<span class="time">'+date+'</span>'
-						+'	</div>'
-						+'</li>'
-					);
-				}
-			});
-	}
+    //滚动时隐藏问题输入框
+    $(window).on('scroll', function () {
+        $('.ask-window').slideUp();
+    });
 });
 
 
--- js/homework-list.js
+++ js/homework-list.js
@@ -4,72 +4,7 @@ $('.back').on('click', function () {
     window.baozitang.close();
 });
 
-var passData = {
-    sectionId: 7,
-    studentId: 3,
-    classId: 1
-}
 
-//请求已完成此节课程作业的学员接口
-getCompetedStu(
-    {
-        'sectionId': passData.sectionId
-    },
-    function (res) {
-        // console.log(res);
-        for (var i = 0; i < res.data.length; i++) {
-            // 头像
-            $('.completed>ul').append(
-                '<li><img src="' + res.data[i].ImgUrl + '" alt=""></li>'
-            );
-        }
-        ;
-        //总人数：
-        $('.completedCount').text(res.data.length);
-    });
 
-//请求作业列表
-getHomeworkList(
-    passData,
-    function (res) {
-        console.log(res);
-        // 错误处理：
-        if (res.code != 200) {
-            mui.toast(res.msg);
-        }
-        // console.log(res);
-        //渲染页面：
-        $('.topic h4').text(res.data.content);
-        for (var i = 0; i < res.data.exerciseList.length; i++) {
-            var data = res.data.exerciseList[i];
-            $('.content ul').append(
-                '<li data-index = "' + i + '">'
-                + '<div class="ques-title">'
-                + '<span>问题 ' + data.exerciseId + '</span>'
-                + '<button></button>'
-                + '</div>'
-                + '<!-- 问题内容 -->'
-                + '<div class="ques-content">'
-                + '<p class="">' + data.workTitle + '</p>'
-                + '</div>'
-                + '</li>'
-            )
-            var button = $('.content ul>li').eq(i).find('.ques-title>button');
-            //作业已完成
-            if (res.data.sectionStatus == 'COMPLETED') {
-                button.css({
-                    'backgroundImage': 'url("../images/end_bj.png")'
-                }).on('click', function () {
-                    window.baozitang.open(jumpBaseUrl + 'homework-preview.html', JSON.stringify(passData));
-                });
-            } else {
-                //作业未完成
-                button.on('click', function () {
-                    passData.index = $(this).parent().parent().attr('data-index');
-                    // window.baozitang.toast(passData.exerciseId);
-                    window.baozitang.open(jumpBaseUrl + 'doing-homework.html', JSON.stringify(passData));
-                });
-            }
-        }
-    });
+
 // })
